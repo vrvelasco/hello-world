59525301321a86355970c251e26c0f0744efba6e
Victor Velasco <victor.velasco@student.swic.edu>
2020-02-01 21:00:32 -0600

    Update for homework Feb 3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // PRIMITIVE data
 
-// We need a way to reuse or access data, so we create a REFERENCE.
-// 'const' alerts JS to reserve some space in memory
-// We provide a name for this space in memory
-// We use the ASSIGNMENT OPERATOR '='
-// This takes the VALUE on the right and assigns to the NAMED REFERENCE.
+/* 
+ * We need a way to reuse or access data, so we create a REFERENCE.
+ * 'const' alerts JS to reserve some space in memory
+ * We provide a name for this space in memory
+ * We use the ASSIGNMENT OPERATOR '='
+ * This takes the VALUE on the right and assigns to the NAMED REFERENCE.
+ */
 const myStringIsThis = "strinadfasdfdsag";
 
 // Use 'let' ONLY if we plan to RE-ASSIGN the VALUE REFERENCED by the VARIABLE.
@@ -13,3 +15,18 @@ let myNum = 23;
 // myStringIsThis = "something else";
 
 console.log(myNum);
+
+// *************************************************************************************
+
+// Homework (is 'var' still used?)
+
+// String
+const myString = "This is a string";
+
+// Number
+let myNumber = 07; // Let's change it
+myNumber = 88;
+
+// Boolean
+let myFlag = False; // Let's change it
+myFlag = True;
