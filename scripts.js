9db2695c562a6999f00ac67f9ac2ac68659c8e23
Victor Velasco <victor.velasco@student.swic.edu>
2020-02-04 20:13:20 -0600

    Add Feb 5 Homework requirements

--- scripts.js
+++ scripts.js
@@ -45,3 +45,52 @@ console.log(parseFloat("123.45")); // 123.45
 console.log(Boolean(222)); // True; anything not zero is true
 console.log("3" * 3); // 9
 console.log("3asdfas" * 3); // NaN
+
+// *************************************************************************************
+// Homework 2/5/2020
+
+const timeOfDay = "Evening"; // String variable
+
+const greeting = `Good ${timeOfDay}!`; // Template literal -> "Good Evening!"
+
+console.log(greeting); // Prints the greeting
+
+let x; // New variable; not assigned
+
+console.log(typeof x); // Undefined??????????????
+
+x = null; // Purposely blank
+
+console.log(typeof x); // Null
+
+console.log(thisVariableDoesNotExist); // Not defined
+
+console.log(greeting); // Won't run because of error
+
+const name = "Victor"; // String variable
+
+let favoriteSong = "The Reason"; // Favorite song
+
+let wage = 1000000; // I wish :)
+
+let age = 31; // My age
+
+const onlyChild = false; // Boolean
+
+let satisfied = true; // Boolean
+
+let favoriteThing = "Video Games"; // String variable
+
+console.log(typeof favoriteThing); // Prints data type
+
+favoriteSong = "En Peligro De Extincion"; // New favorite song
+
+favoriteThing = 42; //THE ANSWER TO LIFE, THE UNIVERSE AND EVERYTHING
+
+age = 32; // On Dec 6th
+
+wage = 2000000; //  Why not?
+
+satisfied = true; // Still true
+
+console.log("My name is " + name + " and my current favorite song is " + favoriteSong + "."); // Sentence using two variables
