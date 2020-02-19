755d013daa0a312e3f8b4e60cd8805fe64099bb9
Victor Velasco <victor.velasco@student.swic.edu>
2020-02-19 13:47:09 -0600

    Update index

--- index.js
+++ index.js
@@ -7,26 +7,85 @@
  * We use the ASSIGNMENT OPERATOR '='
  * This takes the VALUE on the right and assigns to the NAMED REFERENCE.
  */
-const myStringIsThis = "strinadfasdfdsag";
+// const myStringIsThis = "strinadfasdfdsag";
 
 // Use 'let' ONLY if we plan to RE-ASSIGN the VALUE REFERENCED by the VARIABLE.
-let myNum = 23;
+// let myNum = 23;
 
 // myStringIsThis = "something else";
 
-console.log(myNum);
+// console.log(myNum);
 
 // *************************************************************************************
 
 // Homework (is 'var' still used?)
 
 // String
-const myString = "This is a string";
+// const myString = "This is a string";
 
 // Number
-let myNumber = 07; // Let's change it
-myNumber = 88;
+// let myNumber = 07; // Let's change it
+// myNumber = 88;
 
 // Boolean
-let myFlag = false; // Let's change it
-myFlag = true;
+// let myFlag = false; // Let's change it
+// myFlag = true;
+
+// Sum the numbers from 1 to 10.
+let x = 0;
+let total = 0;
+
+while (x != 11) {
+  total += x;
+  x++;
+}
+
+console.log(`Sum of the numbers from 1 to 10: ${total}`);
+
+// Fizz Buzz
+// Log the numbers from 1 to 100.
+// If the number is divisible by 3 and 5, log 'Fizz!Buzz!'
+// However, if the number is divisible by 3, log 'Fizz!'
+// If the number is divisible by 5, log 'Buzz!'
+
+// let num = 1;
+
+// while (num <= 100) {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     console.log(`${num}: Fizz!Buzz!`);
+//   } else if (num % 3 === 0) {
+//     console.log(`${num}: Fizz!`);
+//   } else if (num % 5 === 0) {
+//     console.log(`${num}: Buzz!`);
+//   } else {
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// '!' is the NOT operator. it's a Unary OPERATOR.
+// Using this in front of any data type (such as a number)
+// will immediately convert that data type into a boolean.
+// Then, that value will be inverted as per the NOT operator.
+console.log(!0); // True
+console.log(!111); // False
+console.log(!"Hello"); // False
+console.log(!""); // True
+console.log(!true); // false
+
+// FizzBuzz solution using unary operator.
+let num = 1;
+
+while (num <= 100) {
+  // (!0)
+  if (!(num % 3) && !(num % 5)) {
+    console.log(`${num}: Fizz!Buzz!`);
+  } else if (!(num % 3)) {
+    console.log(`${num}: Fizz!`);
+  } else if (!(num % 5)) {
+    console.log(`${num}: Buzz!`);
+  } else {
+    console.log(num);
+  }
+  num++;
+}
